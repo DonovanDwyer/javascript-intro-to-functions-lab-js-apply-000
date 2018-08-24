47115d432ce6873dbbe93b041fe75e501dd6743d
DONOVAN G DWYER <dwyer.donovan@gmail.com>
2018-08-24 16:20:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string === "HELLO!") {
     console.log('YES INDEED!');
   } else if (string === "hello!") {
-    console.log('I can't hear you!');
+    console.log('I can/'t hear you!');
   } else if (string === "I love you, Grandma.") {
     console.log("I love you, too.")
   }
